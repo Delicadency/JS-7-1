e24023c015a381a309f86923e5996ea8ac20ad02
Agata Kamińska <a.fluffypie@gmail.com>
2024-05-22 12:58:01 +0200

    deleted comment

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-/* Stwórz funkcję fillElement, która otrzyma w argumencie dowolny string. Funkcja ma za zadanie utworzyć element <span> z pomocą document.createElement, umieścić w tym spanie tekst przekazany w parametrze funkcji, oraz ma zwrócić (return) ten element (czyli <span>). */
-
 function fillElement(value) {
   if (typeof value !== "string" || value === "") {
     console.error("Niepoprawny typ danych");
