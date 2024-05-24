f39c744bc529c5184947aff7f34985c783c98d59
Agata Kamińska <a.fluffypie@gmail.com>
2024-05-24 11:35:55 +0200

    added whereToAddSpan and deleted outerHTML in return

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-function fillElement(value) {
+function fillElement(value, tagName) {
   if (typeof value !== "string" || value === "") {
     console.error("Niepoprawny typ danych");
     return;
   }
   const span = document.createElement("span");
   span.innerText = value;
-  const body = document.querySelector("body");
-  body.appendChild(span);
-  return span.outerHTML;
+
+  const whereToAddSpan = document.getElementsByTagName(tagName)[0];
+  if (!whereToAddSpan) {
+    console.error("Nie znaleziono elementu docelowego w drzewie DOM");
+    return;
+  }
+  whereToAddSpan.appendChild(span);
+  return span;
 }
